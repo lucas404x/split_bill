5d18ec0c7eb939faa0b623d29718005ae1d6f0c2
Lucas <joselucas0303@gmail.com>
2020-06-10 20:36:15 -0200

    the class that contains the colors created

---
+++ lib/consts/colors_consts.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorsApp {
+  static const VIEW_COLOR = Color.fromRGBO(14, 24, 34, 1);
+  static const SHADOW_COLOR = Color.fromRGBO(0, 0, 0, 0.25);
+}
