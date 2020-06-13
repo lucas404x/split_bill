cc7ee641108cea4732f2f82ab2b20dfb9c6fe95d
Lucas <joselucas0303@gmail.com>
2020-06-12 23:20:09 -0200

    box_view informations added

--- lib/widgets/box_view.dart
+++ lib/widgets/box_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class BoxView extends StatelessWidget {
+  final Map<String, dynamic> data;
   final double height;
   final Color color;
-  BoxView({@required this.height, @required this.color});
+  BoxView({@required this.data, @required this.height, @required this.color});
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +24,98 @@ class BoxView extends StatelessWidget {
                   height: 0,
                 ),
               )),
-          Container(
-              child: Column(
-            children: <Widget>[],
-          ))
+          Row(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.fromLTRB(35, 30, 10, 30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      data['title'],
+                      style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                    Text(
+                      'R\$${data['value']}',
+                      style: TextStyle(
+                          fontSize: 38,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(30, 30, 5, 20),
+                  child: Row(
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'BILL',
+                            style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          Text(
+                            'FRIENDS',
+                            style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          Text(
+                            'TIPS (${data['tips']}%)',
+                            style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400),
+                          )
+                        ],
+                      ),
+                      Spacer(),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            '\$${data['value']}',
+                            style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          Text(
+                              data['friends'].toString(),
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          
+                          Text(
+                            '${data['increment']}',
+                            style: TextStyle(
+                                fontSize: 13,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 10,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
         ],
       ),
       decoration:
