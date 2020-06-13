b96743ba0eca58c568e0ff1cca961cfc2439777b
Lucas <joselucas0303@gmail.com>
2020-06-12 23:17:05 -0200

    shadow and button color added

--- lib/consts/colors_consts.dart
+++ lib/consts/colors_consts.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class ColorsApp {
   static const VIEW_COLOR = Color.fromRGBO(14, 24, 34, 1);
   static const SHADOW_COLOR = Color.fromRGBO(0, 0, 0, 0.25);
+  static const BUTTON_AND_RESULT_COLOR = Color.fromRGBO(0, 194, 76, 1);
 }
